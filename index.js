8238ef5a1198ab5d8ba5cea4c1d6182bee13665f
William Campbell <campbellwilliam94@gmail.com>
2020-07-06 16:27:29 -0400

    Added calculator functionality, cleaned up styling, need to address chaining operations following order of operations, and limit decimal to one entry. also need to round long decimal results to fit display

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 //DOM manipulation
+const displayText = document.querySelector("p");
 
 //Calculator operation
-memory = [];
-function operate(operation, x, y = memory[-1]) {
-	memory += operation(x, y);
-	return memory[0];
+let operationMemory = [];
+let inputs = "";
+let result = 0;
+
+function operate(operation, x, y = operationMemory[-1]) {
+	result = operation(x, y);
+	displayText.textContent = `${result}`;
 }
 
 function add(x, y) {
@@ -20,17 +24,69 @@ function multiply(x, y) {
 }
 
 function divide(x, y) {
-	return y != 0
-		? x / y
-		: "I know you arent trying to divide by zero on MY CALCULATOR???";
+	return y != 0 ? x / y : "Trying to divide by zero??";
 }
 
 function clear() {
-	memory = [];
+	operationMemory = [];
 }
-console.log(operate(add, 2, 3));
-console.log(operate(add, 2, 3));
-console.log(memory);
 
-clear();
-console.log(memory);
+// Handles number inputs
+document.querySelector(".numbers").addEventListener("click", (e) => {
+	displayText.textContent += `${e.target.textContent}`;
+	if (e.target.textContent == "Clear") {
+		inputs = "";
+	} else inputs += e.target.textContent;
+	console.log(inputs);
+	console.log(operationMemory);
+});
+
+document.querySelector(".functions").addEventListener("click", (e) => {
+	if (inputs != "") {
+		operationMemory.push(parseInt(inputs));
+	}
+
+	displayText.textContent += ` ${e.target.textContent} `;
+	if (e.target.textContent == "+") {
+		operationMemory.push("+");
+		inputs = "";
+		console.log(operationMemory);
+	} else if (e.target.textContent == "-") {
+		operationMemory.push("-");
+		inputs = "";
+		console.log(operationMemory);
+	} else if (e.target.textContent == "X") {
+		operationMemory.push("x");
+		inputs = "";
+		console.log(operationMemory);
+	} else if (e.target.textContent == "/") {
+		operationMemory.push("/");
+		inputs = "";
+		console.log(operationMemory);
+	} else if (e.target.textContent == "=") {
+		console.log(operationMemory);
+		if (operationMemory[1] == "+") {
+			operate(add, operationMemory[0], operationMemory[2]);
+			operationMemory = [result];
+		}
+		if (operationMemory[1] == "-") {
+			operate(subtract, operationMemory[0], operationMemory[2]);
+			operationMemory = [result];
+		}
+		if (operationMemory[1] == "x") {
+			operate(multiply, operationMemory[0], operationMemory[2]);
+			operationMemory = [result];
+		}
+		if (operationMemory[1] == "/") {
+			operate(divide, operationMemory[0], operationMemory[2]);
+			operationMemory = [result];
+		}
+		inputs = "";
+	} else {
+		displayText.textContent = "";
+		inputs = "";
+		operationMemory = [];
+		result = 0;
+	}
+});
+//operationMemory[operationMemory.length - 1] == "Clear" ||
